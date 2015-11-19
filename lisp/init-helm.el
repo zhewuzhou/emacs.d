0f84b03c86695cd5d22ac55344897a824d882fae
zhewuzhou <zhewuzhou@gmail.com>
2015-11-19 17:09:42 +1000

    [JoJo][emacs] set up helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,3 +1,7 @@
 ;; This is the configuration for helm
+(require-package 'helm)
+
+(helm-mode 1)
+(setq helm-buffers-fuzzy-matching t)
 
 (provide 'init-helm)
--- lisp/init-me.el
+++ lisp/init-me.el
@@ -9,9 +9,21 @@
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
 (evil-leader/set-key
+  "." 'switch-to-previous-buffer
+  "x" 'helm-M-x
+  "b" 'helm-mini
+  "f" 'helm-imenu
+  "o" 'delete-other-windows
+  "s" 'delete-trailing-whitespace
   "w" 'save-buffer)
 
 (evilem-default-keybindings "SPC")
+
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
 
 ;;;====================YAS====================
 (yas-global-mode 1)
