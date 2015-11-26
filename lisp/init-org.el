92075e3d76277521513333af8696737b64caa9bf
zhewuzhou <zhewuzhou@gmail.com>
2015-11-26 23:51:55 +1000

    [JoJo][emacs] capture template

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,11 +6,9 @@
            (maybe-require-package 'evil-leader)
            (maybe-require-package 'evil))
 
-  (setq org-agenda-text-search-extra-files '(agenda-archives))
-  (setq org-agenda-files '("~/Dropbox/org/"))
-
   (evil-leader/set-key-for-mode 'org-mode
                                 "I"  'org-time-stamp-inactive
+                                "c"  'org-capture
                                 "S"  'org-time-stamp
                                 "t"  'org-set-tags-command
                                 "T"  'org-todo
@@ -36,6 +34,7 @@
               (evil-define-key 'insert org-mode-map (kbd "C-\\") 'org-insert-heading)
               (auto-fill-mode)
               (flyspell-mode))))
+
 ;;; states and faces
 (setq org-todo-keywords
       (quote ((sequence "TODO(t)" "DOING(n)" "|" "DONE(d)")
@@ -78,6 +77,34 @@
 
 ; For tag searches ignore tasks with scheduled and deadline dates
 (setq org-agenda-tags-todo-honor-ignore-options t)
+
+; org remember stuff
+(setq org-agenda-text-search-extra-files '(agenda-archives))
+(setq org-agenda-files '("~/Dropbox/org/agenda"))
+(setq org-directory "~/Dropbox/org")
+(setq org-default-notes-file "~/Dropbox/org/templates/default-template.org")
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/Dropbox/org/templates/default-template.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/Dropbox/org/templates/default-template.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
 
 (provide 'init-org)
 ;;; init-org.el ends here
