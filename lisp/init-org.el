266ff3dc52ff1f4e41ba2fca0aef0779d77b29d9
zhewuzhou <zhewuzhou@gmail.com>
2015-11-26 22:12:45 +1000

    [JoJo][emacs] rename states makes more sence

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -35,15 +35,15 @@
               (flyspell-mode))))
 ;;; states and faces
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+      (quote ((sequence "TODO(t)" "DOING(n)" "|" "DONE(d)")
+              (sequence "BLOCKED(w@/!)" "REVIEW(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
 
 (setq org-todo-keyword-faces
       (quote (("TODO" :foreground "red" :weight bold)
-              ("NEXT" :foreground "blue" :weight bold)
+              ("DOING" :foreground "blue" :weight bold)
               ("DONE" :foreground "forest green" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
-              ("HOLD" :foreground "magenta" :weight bold)
+              ("BLOCKED" :foreground "orange" :weight bold)
+              ("REVIEW" :foreground "magenta" :weight bold)
               ("CANCELLED" :foreground "forest green" :weight bold)
               ("MEETING" :foreground "forest green" :weight bold)
               ("PHONE" :foreground "forest green" :weight bold))))
