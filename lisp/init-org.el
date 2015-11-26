5b2748a35d28fc54f2d16e74c20daa727d53e9bd
zhewuzhou <zhewuzhou@gmail.com>
2015-11-26 21:55:21 +1000

    [JoJo][emacs] org mode states and faces

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,4 @@
 ;;; init-org.el org plus evil
-;;; Commentary:
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 
@@ -11,15 +10,15 @@
   (setq org-agenda-files '("~/Dropbox/org/"))
 
   (evil-leader/set-key-for-mode 'org-mode
-    "t"  'org-time-stamp-inactive
-    "p"  '(lambda ()
-            (interactive)
-            (org-insert-property-drawer))
-    "d"  'org-deadline
-    "s"  'org-schedule
-    "a"  'org-agenda
-    "ns" 'org-narrow-to-subtree
-    "$"  'org-archive-subtree)
+                                "t"  'org-time-stamp-inactive
+                                "p"  '(lambda ()
+                                        (interactive)
+                                        (org-insert-property-drawer))
+                                "d"  'org-deadline
+                                "s"  'org-schedule
+                                "a"  'org-agenda
+                                "ns" 'org-narrow-to-subtree
+                                "$"  'org-archive-subtree)
 
   (evil-define-key 'normal org-mode-map (kbd "C-<") 'org-metaleft)
   (evil-define-key 'normal org-mode-map (kbd "C->") 'org-metaright)
@@ -34,6 +33,20 @@
               (evil-define-key 'insert org-mode-map (kbd "C-\\") 'org-insert-heading)
               (auto-fill-mode)
               (flyspell-mode))))
+;;; states and faces
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
 
 (provide 'init-org)
 ;;; init-org.el ends here
