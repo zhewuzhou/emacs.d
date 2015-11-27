5c6c500bc7f1792fac14119e3f94f307ff34f747
zhewuzhou <zhewuzhou@gmail.com>
2015-11-28 00:00:01 +1000

    [JoJo][emacs] refile stuff

--- lisp/init-org-capture.el
+++ lisp/init-org-capture.el
@@ -2,31 +2,47 @@
 
 ; org remember stuff
 (setq org-agenda-text-search-extra-files '(agenda-archives))
-(setq org-agenda-files '("~/Dropbox/org/agenda"))
+(setq org-agenda-files '("~/Dropbox/org"))
 (setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file "~/Dropbox/org/templates/default-template.org")
+(setq org-default-notes-file "~/Dropbox/org/refile.org")
 
 ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
 (setq org-capture-templates
-      (quote (("t" "todo" entry (file "~/Dropbox/org/templates/default-template.org")
+      (quote (("t" "todo" entry (file "~/Dropbox/org/refile.org")
                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("r" "respond" entry (file "~/Dropbox/org/templates/default-template.org")
-               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-              ("n" "note" entry (file "~/Dropbox/org/templates/default-template.org")
+              ("r" "respond" entry (file "~/Dropbox/org/refile.org")
+               "* DOING Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/Dropbox/org/refile.org")
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/Dropbox/org/templates/default-template.org")
+              ("j" "Journal" entry (file+datetree "~/Dropbox/org/refile.org")
                "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/Dropbox/org/templates/default-template.org")
+              ("w" "org-protocol" entry (file "~/Dropbox/org/refile.org")
                "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("m" "Meeting" entry (file "~/Dropbox/org/templates/default-template.org")
+              ("m" "Meeting" entry (file "~/Dropbox/org/refile.org")
                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("p" "Phone call" entry (file "~/Dropbox/org/templates/default-template.org")
+              ("p" "Phone call" entry (file "~/Dropbox/org/refile.org")
                "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file "~/Dropbox/org/templates/default-template.org")
-               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+              ("h" "Habit" entry (file "~/Dropbox/org/refile.org")
+               "* DOING %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+;; Remove empty LOGBOOK drawers on clock out
+(defun bh/remove-empty-drawer-on-clock-out ()
+    (interactive) (save-excursion (beginning-of-line 0)
+                (org-remove-empty-drawer-at "LOGBOOK" (point))))
+
+(add-hook 'org-clock-out-hook 'bh/remove-empty-drawer-on-clock-out 'append)
 
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                  (org-agenda-files :maxlevel . 9))))
+
+(setq org-refile-use-outline-path t)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+(defun my/verify-refile-target ()
+    "Exclude todo keywords with a done state from refile targets"
+      (not (member (nth 2 (org-heading-components)) (quote "DONE")))) ;Note - "org-done-keywords"?
+
+(setq org-refile-target-verify-function 'my/verify-refile-target)
 
 (provide 'init-org-capture)
 ;;; init-org.el ends here
