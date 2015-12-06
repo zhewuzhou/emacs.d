2c8b8954d31e1db82e52e03bf6f6ef6f6dbd5940
zhewuzhou <zhewuzhou@gmail.com>
2015-12-07 00:32:10 +1000

    [JoJo][emacs] running R in a seprate window, could be applied to scheme as well

--- lisp/init-r.el
+++ lisp/init-r.el
@@ -1,7 +1,20 @@
 (require-package 'ess)
+(require 'ess-site)
 
 (autoload 'R-mode "ess-site.el" "ESS" t)
 (add-to-list 'auto-mode-alist '("\\.R$" . R-mode))
 (add-to-list 'auto-mode-alist '("\\.r$" . R-mode))
+
+(setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
+(add-hook 'R-mode-hook
+          (lambda()
+            (local-set-key (kbd "M-e")
+                           (lambda ()
+                             (interactive)
+                             (unless (get-buffer "*R*")
+                               (split-window-horizontally-instead)
+                               (R)
+                               (switch-window))
+                             (ess-eval-region (point-min) (point-max) t)))))
 
 (provide 'init-r)
