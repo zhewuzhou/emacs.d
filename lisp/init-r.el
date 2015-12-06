bfb272285ede8c6f3e33d17f954ef217a1a68ca4
zhewuzhou <zhewuzhou@gmail.com>
2015-12-06 20:08:08 +1000

    [JoJo][emacs] auto r mode for r language

--- lisp/init-r.el
+++ lisp/init-r.el
@@ -1,2 +1,7 @@
 (require-package 'ess)
+
+(autoload 'R-mode "ess-site.el" "ESS" t)
+(add-to-list 'auto-mode-alist '("\\.R$" . R-mode))
+(add-to-list 'auto-mode-alist '("\\.r$" . R-mode))
+
 (provide 'init-r)
