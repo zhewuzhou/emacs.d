500fb8fff180a66080ef7926f3f52c3354560626
zhewuzhou <zhewuzhou@gmail.com>
2015-12-07 21:33:58 +1000

    [JoJo][emacs] go company and yasnippet

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,4 +1,14 @@
 (require-package 'go-mode)
+(require-package 'company-go)
 
-(require 'go-mode-autoloads)
+(require 'go-mode)
+(require 'evil)
+(require 'evil-leader)
+
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
+
+(add-hook 'before-save-hook 'gofmt-before-save) ;auto format when save
+
 (provide 'init-go)
