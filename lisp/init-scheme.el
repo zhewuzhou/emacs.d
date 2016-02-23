1954128e59c6f504bbc328c88c2631a78a15773a
zhewuzhou <zhewuzhou@gmail.com>
2016-02-23 23:00:27 +0800

    [Z.Z][Emacs] close the buffer everytime when evaluation

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -2,12 +2,13 @@
 (require 'evil-leader)
 
 (evil-leader/set-key-for-mode 'scheme-mode
-                              "e" '(lambda ()
-                                     (interactive)
-                                     (unless (get-buffer "*scheme*")
-                                       (split-window-horizontally-instead)
-                                       (run-scheme "scheme")
-                                       (switch-window))
-                                     (xscheme-send-buffer)))
+  "e" '(lambda ()
+         (interactive)
+         (if (get-buffer "*scheme*")
+             (kill-buffer "*scheme*"))
+         (split-window-horizontally-instead)
+         (run-scheme "scheme")
+         (switch-window)
+         (xscheme-send-buffer)))
 
 (provide 'init-scheme)
